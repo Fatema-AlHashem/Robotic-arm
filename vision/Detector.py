404885fe1f6c9f44ef8c1e593403437569374233
fatema <162889012+Fatema-AlHashem@users.noreply.github.com>
2026-02-26 22:47:17 +0300

    Update YOLO model path and comments in Detector.py

--- vision/Detector.py
+++ vision/Detector.py
@@ -1,14 +1,15 @@
 from ultralytics import YOLO 
 
-#loads yolo trained model 
+#loads yolo trained model , best.pt contains the weights.
 
-model=YOLO ('yolov8n.pt') #replace with best.pt
+model=YOLO ('model/best.pt') #
 
 #infrenece on the frame ()
 
 def Get_detect(frame):
     results = model(frame) # infrence [x,y ], and conf score x-min/max ,y-min/max
     return results
+
 
 
 
